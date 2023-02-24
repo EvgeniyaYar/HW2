e1bc194189984fb959e76a9cf194cd4ec0f5399c
Evgeniya Yarotskaya <evgeniya047@mail.ru>
2023-02-23 18:09:06 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 18666;
+        int mile = 20;
+
+        int total = amount / mile;
+        System.out.println(total);
+    }
+}
